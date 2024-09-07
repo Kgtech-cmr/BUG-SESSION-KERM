a6d382a0abed8568feea4a2d74dbd3acfda4b415
KermRayan. <kermd237@gmail.com>
2024-09-07 15:19:13 +0100

    SESSION KERM

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 8000;
+const code = require('./pair');
+
+// Set the maximum number of listeners to 500 (optional)
+require('events').EventEmitter.defaultMaxListeners = 500;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the current directory
+app.use('/code', code);
+app.use('/pair', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pair.html'));
+});
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'main.html'));
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`YouTube: https://www.youtube.com/@Darklimule\nTelegram: t.me/darklimule\nGitHub: https://github.com/Limule3650\nInstagram: https://www.instagram.com/mikael_jordan?igsh=MXF3OHgzcnJsZzhqNQ==\n\nServer running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
---
+++ pair.js
@@ -0,0 +1,121 @@
+const express = require('express');
+const fs = require('fs');
+let router = express.Router();
+const pino = require("pino");
+const {
+    default: makeWASocket,
+    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore
+} = require("@whiskeysockets/baileys");
+
+function removeFile(FilePath) {
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true });
+}
+
+router.get('/', async (req, res) => {
+    let num = req.query.number;
+
+    async function XeonPair() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState(`./session`);
+
+        try {
+            let XeonBotInc = makeWASocket({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
+                },
+                printQRInTerminal: false,
+                logger: pino({ level: "fatal" }).child({ level: "fatal" }),
+                browser: ["Ubuntu", "Chrome", "20.0.04"],
+            });
+
+            if (!XeonBotInc.authState.creds.registered) {
+                await delay(1500);
+                num = num.replace(/[^0-9]/g, '');
+                const code = await XeonBotInc.requestPairingCode(num);
+                if (!res.headersSent) {
+                    await res.send({ code });
+                }
+            }
+
+            XeonBotInc.ev.on('creds.update', saveCreds);
+            XeonBotInc.ev.on("connection.update", async (s) => {
+                const { connection, lastDisconnect } = s;
+                if (connection === "open") {
+                    await delay(10000);
+                    const sessionXeon = fs.readFileSync('./session/creds.json');
+                    const audioxeon = fs.readFileSync('./TELESCOPE.mp3');
+                    XeonBotInc.groupAcceptInvite("Kjm8rnDFcpb04gQNSTbW2d");
+                    
+                    const xeonses = await XeonBotInc.sendMessage(XeonBotInc.user.id, { document: sessionXeon, mimetype: `application/json`, fileName: `creds.json` });
+                    await XeonBotInc.sendMessage(XeonBotInc.user.id, {
+                        audio: audioxeon,
+                        mimetype: 'audio/mp4',
+                        ptt: true
+                    }, {
+                        quoted: xeonses
+                    });
+
+                    await XeonBotInc.sendMessage(XeonBotInc.user.id, {
+                        text: `╭═════════════════════⊷
+┃✿︎┃DEATH RAPHAEL SESSION IS 
+┃✿︎┃SUCCESSFULLY
+┃✿︎┃CONNECTED ✅
+╰════════════════════⊷
+╭════════════════════════⊷
+┃Github: https://github.com/Limule3650/Death-Raphael-bug
+
+╰═══════════════════════⊷
+╭════════════════════════⊷
+┃ WaGroup ☘︎ https://chat.whatsapp.com/Bph2Nt7qdqf1p1pQmkIy9U
+
+╰═══════════════════════⊷
+╭════════════════════════⊷
+┃ WHATSAPP CHANNEL ➪https://whatsapp.com/channel/0029Vafhjw0IXnlonRAQMM2l
+
+╰═══════════════════════⊷
+╭═════════════════════════⊷
+┃𝙵𝙾𝙻𝙻𝙾𝚆 𝙼𝚈 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 𝙿𝙻𝙴𝙰𝚂𝙴
+┃𝐖𝐚𝐧𝐭 𝐜𝐡𝐚𝐭 𝐰𝐢𝐭𝐡 𝐦𝐞? ☞︎︎︎ https://Wa.me//+237698581946 ☜︎︎︎
+╰════════════════════════⊷
+©2025 *𝙻𝙸𝙼𝚄𝙻𝙴 𝚂𝙾𝙻𝙸𝚃𝙰𝚁𝚄𝚂*`
+                    }, { quoted: xeonses });
+
+                    await delay(100);
+                    removeFile('./session');
+                    process.exit(0);
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode !== 401) {
+                    await delay(10000);
+                    XeonPair();
+                }
+            });
+        } catch (err) {
+            console.log("Service restarted");
+            await removeFile('./session');
+            if (!res.headersSent) {
+                await res.send({ code: "Service Unavailable" });
+            }
+        }
+    }
+
+    return await XeonPair();
+});
+
+process.on('uncaughtException', function (err) {
+    let e = String(err);
+    if (e.includes("conflict")) return;
+    if (e.includes("Socket connection timeout")) return;
+    if (e.includes("not-authorized")) return;
+    if (e.includes("rate-overlimit")) return;
+    if (e.includes("Connection Closed")) return;
+    if (e.includes("Timed Out")) return;
+    if (e.includes("Value not found")) return;
+    console.log('Caught exception: ', err);
+});
+
+module.exports = router;
