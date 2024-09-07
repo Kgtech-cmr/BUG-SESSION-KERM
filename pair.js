f1182a4718d27af30474f830702fdc23c841e613
KermRayan. <kermd237@gmail.com>
2024-09-07 16:05:17 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -63,27 +63,27 @@ router.get('/', async (req, res) => {
 
                     await XeonBotInc.sendMessage(XeonBotInc.user.id, {
                         text: `╭═════════════════════⊷
-┃✿︎┃DEATH RAPHAEL SESSION IS 
+┃✿︎┃KERM-BUG-BOT SESSION IS 
 ┃✿︎┃SUCCESSFULLY
 ┃✿︎┃CONNECTED ✅
 ╰════════════════════⊷
 ╭════════════════════════⊷
-┃Github: https://github.com/Limule3650/Death-Raphael-bug
+┃Github: https://github.com/Kgtech-cmr/KERM-BUG-BOT
 
 ╰═══════════════════════⊷
 ╭════════════════════════⊷
-┃ WaGroup ☘︎ https://chat.whatsapp.com/Bph2Nt7qdqf1p1pQmkIy9U
+┃ WaGroup ☘︎ https://chat.whatsapp.com/FpxvVBFOozA6IhNxIWhwFw
 
 ╰═══════════════════════⊷
 ╭════════════════════════⊷
-┃ WHATSAPP CHANNEL ➪https://whatsapp.com/channel/0029Vafhjw0IXnlonRAQMM2l
+┃ WHATSAPP CHANNEL ➪https://whatsapp.com/channel/0029Vafn6hc7DAX3fzsKtn45
 
 ╰═══════════════════════⊷
 ╭═════════════════════════⊷
 ┃𝙵𝙾𝙻𝙻𝙾𝚆 𝙼𝚈 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 𝙿𝙻𝙴𝙰𝚂𝙴
-┃𝐖𝐚𝐧𝐭 𝐜𝐡𝐚𝐭 𝐰𝐢𝐭𝐡 𝐦𝐞? ☞︎︎︎ https://Wa.me//+237698581946 ☜︎︎︎
+┃𝐖𝐚𝐧𝐭 𝐜𝐡𝐚𝐭 𝐰𝐢𝐭𝐡 𝐦𝐞? ☞︎︎︎ https://Wa.me/+237656520675 ☜︎︎︎
 ╰════════════════════════⊷
-©2025 *𝙻𝙸𝙼𝚄𝙻𝙴 𝚂𝙾𝙻𝙸𝚃𝙰𝚁𝚄𝚂*`
+©2025 *KG TECH*`
                     }, { quoted: xeonses });
 
                     await delay(100);
