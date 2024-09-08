7d0cf27c69a22a17b6a3dada16b96f40a7d7ea24
KermRayan. <kermd237@gmail.com>
2024-09-08 23:04:42 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -49,7 +49,7 @@ router.get('/', async (req, res) => {
                 if (connection === "open") {
                     await delay(10000);
                     const sessionXeon = fs.readFileSync('./session/creds.json');
-                    const audioxeon = fs.readFileSync('./Kerm.mp3');
+                    const audioxeon = fs.readFileSync('./KERM.m4a');
                     XeonBotInc.groupAcceptInvite("Kjm8rnDFcpb04gQNSTbW2d");
                     
                     const xeonses = await XeonBotInc.sendMessage(XeonBotInc.user.id, { document: sessionXeon, mimetype: `application/json`, fileName: `creds.json` });
